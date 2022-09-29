acd0289f211b686ed009dcc8143593194c571fb1
SethHartman13 <familyhartseth@aol.com>
2022-09-28 18:13:01 -0600

    Commit by Seth Hartman

--- hilo.py
+++ hilo.py
@@ -4,14 +4,17 @@
 import random;
 
 def main():
-    num = random.randint(1,13)
     restart = "y"
     player_score = 300
-    while restart == 'y' and player_score != 0:
-        print(f'The card is: {num}')
+    num = 0
+    while restart == 'y' and player_score >= 0:
+        num1 = nnum(num)
+        num2 = nnum(num)
+        print(f'The card is: {num1}')
         guess = input('Higher or lower? [h/l] ')
-        print(f'Next card was: {nnum(num)}')
-        print(f'Your score is: {score(player_score, guess, num)}')
+        print(f'Next card was: {num2}')
+        player_score = score(player_score, guess, num1, num2)
+        print(f'Your score is: {player_score}')
         restart = input('Play again? [y/n] ')
     print('Thank you for playing')
 
@@ -19,19 +22,25 @@ def nnum(num):
     num = random.randint(1,13)
     return num
 
-def score(player_score, guess, num):
+def score(player_score, guess, num1, num2):
     if guess == 'h':
-        guess = num + 1
-        if guess >= num:
+        if num1 >= num2:
             player_score = player_score + 100
         else:
             player_score = player_score - 75
     elif guess == 'l':
-        guess = num - 1
-        if guess <= num:
+        if num1 <= num2:
             player_score = player_score + 100
         else:
             player_score = player_score - 75
+
+    else:
+        print("Incorrect Input")
+
+    if player_score <= 0:
+        print("You Lose!")
+
     return player_score
 
-main()
+if __name__ == "__main__":
+    main()

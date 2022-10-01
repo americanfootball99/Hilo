76055712aca9d22d021fbfba9c3e13830ad83735
SethHartman13 <familyhartseth@aol.com>
2022-10-01 10:39:18 -0600

    Class Correction

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hilo
+Higher or Lower
+
+Hilo is a guessing game where the objective is to guess if the next number drawn is higher or lower than the previous.
+The game will randomly generate a number and the player guesses 'h' for higher or 'l' for lower.
+If the guess is correct, the player gains 100 points.
+If the guess is incorrect, the player loses 75 points.
+The player starts with 300 points and can keep playing until their score is negative.
+Enjoy
---
+++ hilo copy.py
@@ -0,0 +1,47 @@
+# HILO / Higher or Lower
+# Created by: Logan Simmons and CSE210Thursday@1pm
+
+import random;
+
+class Hilo:
+    def main():
+        restart = "y"
+        player_score = 300
+        num = 0
+        while restart == 'y' and player_score >= 0:
+            num1 = Hilo.nnum(num)
+            num2 = Hilo.nnum(num)
+            print(f'The card is: {num1}')
+            guess = input('Higher or lower? [h/l] ')
+            print(f'Next card was: {num2}')
+            player_score = Hilo.score(player_score, guess, num1, num2)
+            print(f'Your score is: {player_score}')
+            restart = input('Play again? [y/n] ')
+        print('Thank you for playing')
+
+    def nnum(num):
+        num = random.randint(1,13)
+        return num
+
+    def score(player_score, guess, num1, num2):
+        if guess == 'h':
+            if num1 >= num2:
+                player_score = player_score + 100
+            else:
+                player_score = player_score - 75
+        elif guess == 'l':
+            if num1 <= num2:
+                player_score = player_score + 100
+            else:
+                player_score = player_score - 75
+
+        else:
+            print("Incorrect Input")
+
+        if player_score <= 0:
+            print("You Lose!")
+
+        return player_score
+
+if __name__ == "__main__":
+    Hilo.main()
